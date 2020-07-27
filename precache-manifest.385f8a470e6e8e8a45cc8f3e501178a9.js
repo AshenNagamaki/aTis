8598db9daa8a54199354aec6f669d370f5c6a2da
AshenNagamaki <ashennagamaki@protonmail.com>
2020-07-27 17:05:11 +0300

    Updates

---
+++ precache-manifest.385f8a470e6e8e8a45cc8f3e501178a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a889f605607cd15299724bfa88ba3fd6",
+    "url": "/aTis/index.html"
+  },
+  {
+    "revision": "585ab5b275413be18278",
+    "url": "/aTis/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "23208fe12d4edbf254f1",
+    "url": "/aTis/static/css/main.ae893e2b.chunk.css"
+  },
+  {
+    "revision": "585ab5b275413be18278",
+    "url": "/aTis/static/js/2.d64bca6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aTis/static/js/2.d64bca6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23208fe12d4edbf254f1",
+    "url": "/aTis/static/js/main.fe13f5e7.chunk.js"
+  },
+  {
+    "revision": "5f72b667bf62428070f4",
+    "url": "/aTis/static/js/runtime-main.b03f1f6a.js"
+  },
+  {
+    "revision": "e6e4d7b7f4b5a5585b48c841e5eb5572",
+    "url": "/aTis/static/media/outcomeImage.e6e4d7b7.png"
+  }
+]);
